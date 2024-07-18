22c0fd821a048bb02d0d89fc97fcc9aaedbf7987
DjontLong <web.steamflower@gmail.com>
2024-07-18 16:50:36 +0700

    Create methods displayDirectoryContent, handleCommand

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        SimpleFileManager.printMenu();
-        System.out.println();
+    public static void main(String[] args) throws IOException {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                SimpleFileManager.printMenu();
+                if (scanner.hasNextInt()) {
+                    int command = Integer.parseInt(scanner.nextLine());
+                    if (command < 0 || command > 5) {
+                        System.out.println("Введена неправильная команда. Повторите попытку!");
+                        continue;
+                    }
+
+                    if (command == 0) {
+                        break;
+                    }
+
+                    SimpleFileManager.handleCommand(command, scanner);
+                } else {
+                    System.out.println("Введена неправильная команда. Повторите попытку!");
+                    scanner.nextLine();
+                }
+            }
+        }
     }
 }
--- src/SimpleFileManager.java
+++ src/SimpleFileManager.java
@@ -1,4 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Scanner;
+
 public class SimpleFileManager {
+    // Создадим константу, которая будет содержать путь директории по умолчанию
+    private static Path currentDirectory = Paths.get(".");
+
     public static void printMenu() {
         System.out.println("┌──────────────────────────────────────────────────────────────────────────────┐");
         System.out.println("│        Файловый менеджер                                                     │");
@@ -6,5 +14,56 @@ public class SimpleFileManager {
         System.out.println("│ 1 - Добавить папку    2 - Добавить файл    3 - Удалить    4 - Переименовать  │");
         System.out.println("│ 5 - Просмотр          0 - Выйти                                              │");
         System.out.println("└──────────────────────────────────────────────────────────────────────────────┘");
+        System.out.print("Введите команду: ");
+    }
+
+    public static void handleCommand(int command, Scanner scanner) {
+        switch (command) {
+            case 1:
+                System.out.print("Введите имя новой папки: ");
+                String newNameDir = scanner.nextLine();
+
+                try {
+                    Path newDirPath = currentDirectory.resolve(newNameDir);
+                    Files.createDirectory(newDirPath);
+                } catch (InvalidPathException | IOException e) {
+                    System.out.println("Ошибка: " + e.getMessage() + "\n" + "Повторите попытку!");
+                }
+                break;
+
+            case 2:
+
+
+            case 5:
+                System.out.print("Для просмотра текущей директории нажмите <.>, либо введите путь: ");
+                String choice = scanner.nextLine();
+
+                if (choice.equals(".")) {
+                    displayDirectoryContent(currentDirectory);
+                } else {
+                    Path path = Path.of(choice);
+                    if (Files.exists(path)) {
+                        displayDirectoryContent(path);
+                        currentDirectory = path;
+                    } else {
+                        System.out.println("Указанной директории не существует! Повторите попытку!");
+                    }
+                }
+                break;
+        }
+    }
+
+    public static void displayDirectoryContent(Path path) {
+        try (DirectoryStream<Path> displayDirectory = Files.newDirectoryStream(path)) {
+            for (Path file : displayDirectory) {
+                if (Files.isRegularFile(file)) {
+                    System.out.println("[FILE] " + file.getFileName());
+                } else if (Files.isDirectory(file)) {
+                    System.out.println("[DIR] " + file.getFileName());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
